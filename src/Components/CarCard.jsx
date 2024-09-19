5667761059ab30657f03aaf8cd9e66252a63c36f
Favour264 <adamsfavour264@gmail.com>
2024-09-19 11:56:04 -0700

    fourth commit

---
+++ src/Components/CarCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CarCard = ({ image, name, price, type, capacity, fuel, transmission }) => {
+  return (
+    <div className="border rounded-lg shadow-lg p-4 max-w-sm">
+      <img src={image} alt={name} className="mt-20 mx-auto object-cover rounded" />
+      <h2 className="text-xl font-bold mt-2">{name}</h2>
+      <p className="text-gray-600">{type}</p>
+      <div className="flex justify-between mt-2">
+        <p className="text-lg font-bold">${price}/day</p>
+        <button className="bg-blue-500 text-white py-1 px-4 rounded"><Link to="/Bill">Rental Car </Link></button>
+      </div>
+      <div className="flex justify-between mt-2 text-gray-500 text-sm">
+        <p>{fuel}</p>
+        <p>{transmission}</p>
+        <p>{capacity} People</p>
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
